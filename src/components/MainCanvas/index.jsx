506b5b21f79d97c643cb4a3e1e7e549f0fd9a541
Andrey <1.andrey.p.1999@gmail.com>
2024-04-28 15:12:23 +0300

    bug fixes2

--- src/components/MainCanvas/index.jsx
+++ src/components/MainCanvas/index.jsx
@@ -5,7 +5,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 import style from "./style.module.css";
 import { OrbitControls, Stage } from "@react-three/drei";
+import { useEffect } from "react";
 export const MainCanvas = ({ file }) => {
+
     let gltf = useLoader(GLTFLoader, '/3d_Model/' + file);
     return (
         <div id="root" style={{ position: "relative" }} className={style.canvas}>
@@ -14,8 +16,8 @@ export const MainCanvas = ({ file }) => {
                 <directionalLight color="red" position={[0, 5, 0]} />
                 <ambientLight color="white" />
                 <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 3.2} maxPolarAngle={Math.PI / 2.2} />
-                <Stage adjustCamera intensity={4} shadows="contact" >
-                    <primitive object={gltf.scene} scale={0.1} />
+                <Stage adjustCamera={false} intensity={2} shadows="contact" >
+                    <primitive object={gltf.scene} scale={0.2} />
                 </Stage>
             </Canvas>
         </div>
